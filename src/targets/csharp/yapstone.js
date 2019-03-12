2f2fbc97ced9d501b77f0113cdb3803d38642391
Ashwin Vaswani <ashwinv11@gmail.com>
2019-03-11 20:04:28 -0700

    oas 3 support for node and c#, pulling data from examples

--- src/targets/csharp/yapstone.js
+++ src/targets/csharp/yapstone.js
@@ -1,73 +1,99 @@
-'use strict'
+"use strict";
 
-var camelcase = require('camelcase')
-var util = require('util')
-var CodeBuilder = require('../../helpers/code-builder')
+var camelcase = require("camelcase");
+var util = require("util");
+var CodeBuilder = require("../../helpers/code-builder");
 
-module.exports = function (source, options) {
-  var code = new CodeBuilder()
+module.exports = function(source, options) {
+  var code = new CodeBuilder();
 
-  code.push('// Setup')
-      .push("using Yapstone.Api;")
-      .push("using Yapstone.Client;")
-      .push("using Yapstone.Model;")
-      // .blank()
-      // .push('Configuration.Default.ApiKey.Add("Authorization", "%s");', "YOUR_API_KEY")
-      // .push('Configuration.Default.ApiKeyPrefix.Add("Authorization", "Bearer");')
-      .blank()
+  code
+    .push("// Setup")
+    .push("using Yapstone.Api;")
+    .push("using Yapstone.Client;")
+    .push("using Yapstone.Model;")
+    // .blank()
+    // .push('Configuration.Default.ApiKey.Add("Authorization", "%s");', "YOUR_API_KEY")
+    // .push('Configuration.Default.ApiKeyPrefix.Add("Authorization", "Bearer");')
+    .blank();
 
-  var lastSwaggerUri = source['_swaggerSettings']
-                          .path.split('/').filter(function(el) {
-                            return !el.includes('{')
-                          }).pop()
+  var lastSwaggerUri = source["_swaggerSettings"].path
+    .split("/")
+    .filter(function(el) {
+      return !el.includes("{");
+    })
+    .pop();
 
-  var swaggerMethod = source['_swaggerSettings'].originalMethod
-  var operationId = camelcase(swaggerMethod.operationId, { pascalCase: true })
+  var swaggerMethod = source["_swaggerSettings"].originalMethod;
+  var operationId = camelcase(swaggerMethod.operationId, { pascalCase: true });
 
-  var apiName = camelcase(lastSwaggerUri + '-api', { pascalCase: true })
-  var paramNames = []
+  var apiName = camelcase(lastSwaggerUri + "-api", { pascalCase: true });
+  var paramNames = [];
 
-  code.push('var api = new %s();', apiName)
+  code.push("var api = new %s();", apiName);
 
   // Deal with path parameters
-  if(source['_swaggerSettings'].pathParams.length > 0) {
-    paramNames = paramNames.concat(source['_swaggerSettings'].pathParams.map(function(param) {
-      code.push('var %s = "%s";', param.name, param.value)
+  if (source["_swaggerSettings"].pathParams.length > 0) {
+    paramNames = paramNames.concat(
+      source["_swaggerSettings"].pathParams.map(function(param) {
+        code.push('var %s = "%s";', param.name, param.value);
 
-      if(param.name != null) {
-        return param.name
-      }
-    }))
+        if (param.name != null) {
+          return param.name;
+        }
+      })
+    );
   }
 
   // Deal with body params
-  if(source.postData.mimeType === 'application/json') {
-    paramNames = paramNames.concat(swaggerMethod.parameters.map(function(param) {
-      if(param.in === 'body') {
-        code.push('var %s = new %s();', param.name, camelcase(param.name, { pascalCase: true }))
-        return param.name
-      }
-    }))
+  // if (source.postData.mimeType === "application/json") {
+  //   paramNames = paramNames.concat(
+  //     swaggerMethod.parameters.map(function(param) {
+  //       if (param.in === "body") {
+  //         code.push(
+  //           "var %s = new %s();",
+  //           param.name,
+  //           camelcase(param.name, { pascalCase: true })
+  //         );
+  //         return param.name;
+  //       }
+  //     })
+  //   );
+  // }
+
+  if (source.postData.mimeType === "application/json") {
+    if (source.postData.jsonObj) {
+      code.push(
+        "var %s = new %s();",
+        "requestBody",
+        camelcase("requestBody", { pascalCase: true })
+      );
+    }
+    paramNames.push("requestBody");
   }
 
   var filteredParamNames = paramNames.filter(function(el) {
     return el != null;
-  })
+  });
 
-  if(filteredParamNames.length > 0) {
-    code.blank()
-        .push('var result = api.%s(%s);', operationId, filteredParamNames.join(', '))
+  if (filteredParamNames.length > 0) {
+    code
+      .blank()
+      .push(
+        "var result = api.%s(%s);",
+        operationId,
+        filteredParamNames.join(", ")
+      );
   } else {
-    code.blank()
-        .push('var result = api.%s();', operationId)
+    code.blank().push("var result = api.%s();", operationId);
   }
 
-  return code.join()
-}
+  return code.join();
+};
 
 module.exports.info = {
-  key: 'yapstone',
-  title: 'Yapstone',
-  link: 'http://restsharp.org/',
-  description: 'Yapstone API Client for .NET'
-}
+  key: "yapstone",
+  title: "Yapstone",
+  link: "http://restsharp.org/",
+  description: "Yapstone API Client for .NET"
+};
--- src/targets/node/yapstone.js
+++ src/targets/node/yapstone.js
@@ -8,18 +8,21 @@
  * for any questions or issues regarding the generated code snippet, please open an issue mentioning the author.
  */
 
-'use strict'
+"use strict";
 
-var util = require('util')
-var camelcase = require('camelcase')
-var CodeBuilder = require('../../helpers/code-builder')
+var util = require("util");
+var camelcase = require("camelcase");
+var CodeBuilder = require("../../helpers/code-builder");
 
-module.exports = function (source, options) {
-  var opts = util._extend({
-    indent: '  '
-  }, options)
+module.exports = function(source, options) {
+  var opts = util._extend(
+    {
+      indent: "  "
+    },
+    options
+  );
 
-  var code = new CodeBuilder(opts.indent)
+  var code = new CodeBuilder(opts.indent);
 
   var reqOpts = {
     method: source.method,
@@ -27,81 +30,88 @@ module.exports = function (source, options) {
     port: source.uriObj.port,
     path: source.uriObj.path,
     headers: source.allHeaders
-  }
+  };
 
-  var lastSwaggerUri = source['_swaggerSettings']
-                          .path.split('/').filter(function(el) {
-                            return !el.includes('{')
-                          }).pop()
+  var lastSwaggerUri = source["_swaggerSettings"].path
+    .split("/")
+    .filter(function(el) {
+      return !el.includes("{");
+    })
+    .pop();
 
-  var swaggerMethod = source['_swaggerSettings'].originalMethod
-  var operationId = swaggerMethod.operationId
+  var swaggerMethod = source["_swaggerSettings"].originalMethod;
+  var operationId = swaggerMethod.operationId;
 
-  var apiName = camelcase(lastSwaggerUri + '-api', { pascalCase: true })
-  var paramNames = []
+  var apiName = camelcase(lastSwaggerUri + "-api", { pascalCase: true });
+  var paramNames = [];
 
-  code.push("import * as Yapstone from 'yapstone-js';")
+  code.push("import * as Yapstone from 'yapstone-js';");
 
-  code.blank()
-      // .push('// Setup')
-      .push('const client = Yapstone.ApiClient.instance;')
-      // .push('const Bearer = client.authentications["Bearer"];')
-      // .push('Bearer.apiKey = "YOUR_API_KEY";')
-      // .blank()
-      .push('const api = new Yapstone.%s();', apiName)
-      .blank()
-      .push('async function %s() {', operationId)
+  code
+    .blank()
+    // .push('// Setup')
+    .push("const client = Yapstone.ApiClient.instance;")
+    // .push('const Bearer = client.authentications["Bearer"];')
+    // .push('Bearer.apiKey = "YOUR_API_KEY";')
+    // .blank()
+    .push("const api = new Yapstone.%s();", apiName)
+    .blank()
+    .push("async function %s() {", operationId);
 
   // Deal with path parameters
-  if(source['_swaggerSettings'].pathParams.length > 0) {
-    paramNames = paramNames.concat(source['_swaggerSettings'].pathParams.map(function(param) {
-      code.push(1, 'const %s = "%s";', param.name, param.value)
+  if (source["_swaggerSettings"].pathParams.length > 0) {
+    paramNames = paramNames.concat(
+      source["_swaggerSettings"].pathParams.map(function(param) {
+        code.push(1, 'const %s = "%s";', param.name, param.value);
 
-      if(param.name != null) {
-        return param.name
-      }
-    }))
+        if (param.name != null) {
+          return param.name;
+        }
+      })
+    );
   }
 
   // Deal with body params
-  if(source.postData.mimeType === 'application/json') {
-    paramNames = paramNames.concat(swaggerMethod.parameters.map(function(param) {
-      if(param.in === 'body') {
-        var bodyExample = param.schema && param.schema.example || source.postData.jsonObj;
-        code.push(1, 'const %s = %s;', param.name, util.inspect(bodyExample, {
+  if (source.postData.mimeType === "application/json") {
+    if (source.postData.jsonObj) {
+      code.push(
+        1,
+        "const %s = %s;",
+        "requestBody",
+        util.inspect(source.postData.jsonObj, {
           depth: null
-        }))
-        return param.name
-      }
-    }))
+        })
+      );
+    }
+    paramNames.push("requestBody");
   }
 
-  code.blank()
-      .push(1, 'return await api')
+  code.blank().push(1, "return await api");
 
   var filteredParamNames = paramNames.filter(function(el) {
     return el != null;
-  })
+  });
 
-  if(filteredParamNames.length > 0) {
-    code.push(2, '.%s(%s)', operationId, filteredParamNames.join(', '))
+  if (filteredParamNames.length > 0) {
+    code.push(2, ".%s(%s)", operationId, filteredParamNames.join(", "));
   } else {
-    code.push(2, '.%s()', operationId)
+    code.push(2, ".%s()", operationId);
   }
 
-  code.push(2, '.catch(err => {')
-      .push(3, 'console.error(err);')
-      .push(2, '});')
-      .push('};')
-      .blank()
-      .push('console.log(%s());', operationId)
+  code
+    .push(2, ".catch(err => {")
+    .push(3, "console.error(err);")
+    .push(2, "});")
+    .push("};")
+    .blank()
+    .push("console.log(%s());", operationId);
 
-  return code.join()
-}
+  return code.join();
+};
 
 module.exports.info = {
-  key: 'yapstone',
-  title: 'HTTP',
-  link: 'http://nodejs.org/api/http.html#http_http_request_options_callback',
-  description: 'Yapstone JS interface for Node.js'
-}
+  key: "yapstone",
+  title: "HTTP",
+  link: "http://nodejs.org/api/http.html#http_http_request_options_callback",
+  description: "Yapstone JS interface for Node.js"
+};
